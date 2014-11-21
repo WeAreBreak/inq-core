e4502f5e4043ad96563d8756c9524b591d43dd06
Balázs Schwarzkopf <balazs.schwarzkopf@gmail.com>
2014-11-21 16:45:38 +0100

    Query able is under develpment

--- lab/query.js
+++ lab/query.js
@@ -4,18 +4,41 @@
 
 require('..')
 
+var fs = require('fs')
+
 $(function* () {
-    var query, nums = [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
+    var query,
+        nums  = [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ],
+        files = [ 'chain.js', 'query.js', 'this.js', 'yield.js' ]
 
-    query = nums.$
-        .where(function (n) {
-            return n > 5
+    //query = $
+    //    .from(nums)
+    //    .where(function (n) {
+    //        return n > 5
+    //    })
+    //    .select(function (n) {
+    //        return {
+    //            id: n * n
+    //        }
+    //    })
+    //    .last(function (item) {
+    //        return item.id < 60
+    //    })
+    //    .first()
+    //
+    //console.log((yield query.exec()) - 7)
+
+    query =
+       $.from(files)
+        .map(function (item) {
+            return __dirname + '/' + item
         })
-        .select(function (n) {
-            return {
-                id: n * n
-            }
+        .map(function* (file) {
+            return yield fs.readFile.$(file, { encoding: 'utf8' })
+        })
+        .select(function (content) {
+            return content.replace(/\n|\t|\r/g, '').replace(/\s{2,}/g, ' ')
         })
 
-    console.log(yield query.exec())
+    console.log(yield query.exec());
 })
